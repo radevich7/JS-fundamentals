6a87c3c466307970e17c628c487c34be62e358f0
radevich7 <radevich7@gmail.com>
2021-10-12 09:13:40 -0600

    <practise with parseFloat(value)>

--- code.js
+++ code.js
@@ -1,17 +1,25 @@
 "useStrict";
+// // === 5 ===
+// Create a function to convert an array of percentages to their decimal equivalents.
+function convertToDecimal(perc) {
+  return perc.map((val) => parseFloat(val) / 100);
+}
+
+console.log(convertToDecimal(["1%", "2%", "3%"]));
+
 // // === 4 ===
 // Create a function that takes a string str and performs simple encoding as per the following method:
 
 // Replace all single occurrence characters with [
 // Replace all characters with two or more occurrences with ]
 // Return the final string after modification.
-function simpleEncoder(str) {
-  str = str.toLowerCase().split("");
-  return str
-    .map((el, i) => (str.indexOf(el) == str.lastIndexOf(el) ? "[" : "]"))
-    .join("");
-}
-console.log(simpleEncoder("Muubashirr"));
+// function simpleEncoder(str) {
+//   str = str.toLowerCase().split("");
+//   return str
+//     .map((el, i) => (str.indexOf(el) == str.lastIndexOf(el) ? "[" : "]"))
+//     .join("");
+// }
+// console.log(simpleEncoder("Muubashirr"));
 
 // // === 3 ===
 // // Given an object of how many more pages each ink color can print, output the maximum number of pages the printer can print before any of the colors run out.
