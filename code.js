3df40ddb974c3afd254bef51d6704b851b3ae8e8
radevich7 <radevich7@gmail.com>
2021-10-20 08:48:41 -0600

    <comparison practise>

--- code.js
+++ code.js
@@ -1,4 +1,16 @@
 "useStrict";
+//===22===
+// Steve and Maurice have racing snails. They each have three, a slow s, medium m and fast f one. Although Steve's snails are all a bit stronger than Maurice's, Maurice has a trick up his sleeve. His plan is
+
+// Round 1: [s, f] Sacrifice his slowest snail against Steve's fastest.
+// Round 2: [m, s] Use his middle snail against Steve's slowest.
+// Round 3: [f, m] Use his fastest snail against Steve's middle.
+// Create a function that determines whether Maurice's plan will work by outputting true if Maurice wins 2/3 games.
+function mauriceWins(mS, sS) {
+  return mS[1] > sS[0] && mS[2] > sS[1];
+}
+console.log(mauriceWins([3, 8, 10], [4, 7, 11]));
+
 //===21===
 // Create a function that returns true if the first array is a subset of the second. Return false otherwise.
 // function isSubset(arr1, arr2) {
