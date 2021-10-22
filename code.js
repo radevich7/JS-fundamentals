f4d30bc2a6b9270e7458462c00e2d2dbbd296e44
radevich7 <radevich7@gmail.com>
2021-10-22 08:23:14 -0600

    <object.entries and object.keys and object .values practise.>

--- code.js
+++ code.js
@@ -1,13 +1,23 @@
 "useStrict";
-// ===33===
-// Given a word, write a function that returns the first index and the last index of a character.
-function charIndex(word, char) {
-  let first = word.indexOf(char);
-  let last = word.lastIndexOf(char);
-  return first === -1 || last === -1 ? undefined : [first, last];
+// ===34===
+// Create a function that takes an object and returns the keys and values as separate arrays. Return the keys sorted alphabetically, and their corresponding values in the same order.
+function keysAndValues(obj) {
+  let keys = Object.keys(obj);
+  let values = Object.values(obj);
+  return [keys, values];
 }
 
-console.log(charIndex("hello", "l"));
+console.log(keysAndValues({ a: 1, b: 2, c: 3 }));
+
+// ===33===
+// Given a word, write a function that returns the first index and the last index of a character.
+// function charIndex(word, char) {
+//   let first = word.indexOf(char);
+//   let last = word.lastIndexOf(char);
+//   return first === -1 || last === -1 ? undefined : [first, last];
+// }
+
+// console.log(charIndex("hello", "l"));
 // ===32===
 // Write a function that converts an object into an array, where each element represents a key-value pair in the form of an array.
 // function toArray(obj) {
