12a56b12cfd0dd18635fcb4a87e6c8c0837ca1c9
Julian Radevych <79290689+radevich7@users.noreply.github.com>
2021-10-26 08:46:26 -0600

    <true false statement practise >

--- code.js
+++ code.js
@@ -1,4 +1,18 @@
 "useStrict";
+// ===41===
+// Given a total due and an array representing the amount of change in your pocket, determine whether or not you are able to pay for the item. Change will always be represented in the following order: quarters, dimes, nickels, pennies.
+
+// To illustrate: changeEnough([25, 20, 5, 0], 4.25) should yield true, since having 25 quarters, 20 dimes, 5 nickels and 0 pennies gives you 6.25 + 2 + .25 + 0 = 8.50.
+function changeEnough(change, amountDue) {
+  return 0.25 * change[0] +
+    0.1 * change[1] +
+    0.05 * change[2] +
+    0.01 * change[3] >=
+    amountDue
+    ? true
+    : false;
+}
+console.log(changeEnough([2, 100, 0, 0], 14.11));
 // ===40===
 // Create a function that counts the integer's number of digits.
 // function count(n) {
