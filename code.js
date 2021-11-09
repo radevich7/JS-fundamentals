b6355dd139cb3fddf66e8047e30a91eb21445bdc
Julian Radevych <79290689+radevich7@users.noreply.github.com>
2021-11-09 09:34:39 -0700

    <reduce method practise>

--- code.js
+++ code.js
@@ -1,4 +1,17 @@
 "useStrict";
+// // ===77===
+// Given an array of boxes, create a function that returns the total volume of all those boxes combined together. A box is represented by an array with three elements: length, width and height.
+
+// For instance, totalVolume([2, 3, 2], [6, 6, 7], [1, 2, 1]) should return 266 since (2 x 3 x 2) + (6 x 6 x 7) + (1 x 2 x 1) = 12 + 252 + 2 = 266.
+// let sum;
+// function totalVolume(...boxes) {
+//   return boxes.reduce(
+//     (prev, cur) => prev + cur.reduce((prev, cur) => prev * cur, 1),
+//     0
+//   );
+// }
+// console.log(totalVolume([4, 2, 4], [3, 3, 3], [1, 1, 2], [2, 1, 1]));
+
 // ===76===
 // Write a function that takes an integer i and returns an integer with the integer backwards followed by the original integer.
 // function reverseAndNot(i) {
