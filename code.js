e7155b875b939f61c71c58c14686213e0ab227dc
Julian Radevych <79290689+radevich7@users.noreply.github.com>
2021-11-17 09:02:39 -0700

    <regex practise>

--- code.js
+++ code.js
@@ -1,10 +1,17 @@
 "useStrict";
+// ===97===
+// Create a function which counts how many lone 1s appear in a given number. Lone means the number doesn't appear twice or more in a row.
+function countLoneOnes(n) {
+  return ((n + "").match(/(?<!1)1(?!1)/g) || []).length;
+}
+console.log(countLoneOnes(101));
+
 // ===96===
 // Create a function that takes an array of items, removes all duplicate items and returns a new array in the same sequential order as the old array (minus duplicates).
-function removeDups(arr) {
-  return Array.from(new Set(arr));
-}
-console.log(removeDups(["John", "Taylor", "John"]));
+// function removeDups(arr) {
+//   return Array.from(new Set(arr));
+// }
+// console.log(removeDups(["John", "Taylor", "John"]));
 // ===95===
 // Arrays can be mixed with various types. Your task for this challenge is to sum all the number elements in the given array. Create a function that takes an array and returns the sum of all numbers in the array.
 // function numbersSum(arr) {
