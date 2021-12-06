fdcf15f39546541b1d2b574b9bd386bffc89651f
Julian Radevych <79290689+radevich7@users.noreply.github.com>
2021-12-06 09:46:49 -0700

    <split and push method practise>

--- code.js
+++ code.js
@@ -1,4 +1,39 @@
 "useStrict";
+// ===142===
+// Parity bits are used as a very simple checksum to ensure that binary data isn't corrupted during transit. Here's how they work:
+
+// If a binary string has an odd number of 1's, the parity bit is a 1.
+// If a binary string has an even number of 1's, the parity bit is a 0.
+// The parity bit is appended to the end of the binary string.
+// Create a function that adds the correct parity bit to a binary string.
+
+// Examples
+// addParityBit("1011011") ➞ "10110111"
+// // There are five 1's.
+// // Since five is odd, the parity bit should be a 1.
+// // Add the parity bit to the end of the string.
+// // Return the result.
+
+// addParityBit("0110000") ➞ "01100000"
+
+// addParityBit("0101101") ➞ "01011010"
+
+// addParityBit("1111111") ➞ "11111111"
+// Notes
+// All inputs will be 7-bits long (so that the parity bit makes it a full byte).
+
+const parity = (string) => {
+  let array = string.split("");
+  let first = array.filter((x) => x == 1).length;
+  if (first % 2 == 0) {
+    array.push("0");
+  } else array.push("1");
+
+  return array.join("");
+};
+
+console.log(parity("0110000"));
+
 // ===141===
 // In this challenge you will receive an input of the form:
 
