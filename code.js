5921dbb5e3ed817d2e4828927d024e1f64a77b8f
Julian Radevych <79290689+radevich7@users.noreply.github.com>
2021-12-16 09:44:06 -0700

    <number%10 will give last digits>

--- code.js
+++ code.js
@@ -1,4 +1,31 @@
 // "useStrict";
+// ===155===
+// Your job is to create a function, that takes 3 numbers: a, b, c and returns true if the last digit of a * b = the last digit of c. Check the examples below for an explanation.
+
+// Examples
+// lastDig(25, 21, 125) ➞ true
+// // The last digit of 25 is 5, the last digit of 21 is 1, and the last
+// // digit of 125 is 5, and the last digit of 5*1 = 5, which is equal
+// // to the last digit of 125(5).
+
+// lastDig(55, 226, 5190) ➞ true
+// // The last digit of 55 is 5, the last digit of 226 is 6, and the last
+// // digit of 5190 is 0, and the last digit of 5*6 = 30 is 0, which is
+// // equal to the last digit of 5190(0).
+
+// lastDig(12, 215, 2142) ➞ false
+// // The last digit of 12 is 2, the last digit of 215 is 5, and the last
+// // digit of 2142 is 2, and the last digit of 2*5 = 10 is 0, which is
+// // not equal to the last digit of 2142(2).
+// Notes
+// Numbers can be negative.
+
+// const lastDigit=(a,b,c)=>{
+//     console.log(a%10)
+//   	return ((a%10)*(b%10))%10 === c%10
+// }
+
+// console.log(lastDigit(25, 21, 125))
 // ===154===
 // You will be given a string of characters containing only the following characters: ():
 
@@ -27,38 +54,43 @@
 //  if there is any other input return 0
 // return total
 
-const happyNumber = (str) => {
-  let split = str.split("");
+// const happyNumber = (str) => {
+//   let split = str.split("");
 
-  let countSmiley = 0;
-  let countSadly = 0;
-  for (let i = 0; i < split.length; i++) {
-    //  ")"
-    if (split[i] === ")") {
-      if (split[i - 1] === ":") {
-        countSmiley++;
-      }
-      if (split[i + 1] === ":") {
-        countSadly--;
-      }
-    }
-    // "("
+// let count=0;
 
-    if (split[i] === "(") {
-      if (split[i - 1] === ":") {
-        countSadly--;
-      }
-      if (split[i + 1] === ":") {
-        countSmiley++;
-      }
-    }
-  }
-  return countSadly + countSmiley;
+// // let checker=split.map((val, index)=> {val===')'&&val[index-1]===':' ? count++ :  val===')'&&val[index+1]===':' ? count-- :
+// // val==='('&&val[index-1]===':' ? count-- :  val==='('&&val[index+1]===':' ? count+
 
-  //
-};
+// let countSmiley = 0;
+// let countSadly = 0;
+// for (let i = 0; i < split.length; i++) {
+//   //  ")"
+//   if (split[i] === ")") {
+//     if (split[i - 1] === ":") {
+//       countSmiley++;
+//     }
+//     if (split[i + 1] === ":") {
+//       countSadly--;
+//     }
+//   }
+//   // "("
 
-console.log(happyNumber(":::()):)"));
+//   if (split[i] === "(") {
+//     if (split[i - 1] === ":") {
+//       countSadly--;
+//     }
+//     if (split[i + 1] === ":") {
+//       countSmiley++;
+//     }
+//   }
+// }
+// return countSadly + countSmiley;
+
+// //
+// }
+
+// console.log(happyNumber(":::()):)"));
 
 // ===153===
 // Given an integer array, transform that array into a mirror.
