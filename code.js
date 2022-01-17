2bcf8719adf65f1505224712f041703a528c3f06
Julian Radevych <79290689+radevich7@users.noreply.github.com>
2022-01-17 08:20:10 -0700

    <loop practise>

--- code.js
+++ code.js
@@ -1,4 +1,43 @@
 "use strict";
+// ===169===
+// When coloring a striped pattern, you may start by coloring each square sequentially, meaning you spend time needing to switch coloring pencils.
+
+// Create a function where given an array of colors cols, return how long it takes to color the whole pattern. Note the following times:
+
+// It takes 1 second to switch between pencils.
+// It takes 2 seconds to color a square.
+// See the example below for clarification.
+
+// color_pattern_times(["Red", "Blue", "Red", "Blue", "Red"]) ➞ 14
+
+// // There are 5 colors so it takes 2 seconds to color each one (2 x 5 = 10).
+// // You need to switch the pencils 4 times and it takes 1 second to switch (1 x 4 = 4).
+// // 10 + 4 = 14
+// Examples
+// colorPatternTimes(["Blue"]) ➞ 2
+
+// colorPatternTimes(["Red", "Yellow", "Green", "Blue"]) ➞ 11
+
+// colorPatternTimes(["Blue", "Blue", "Blue", "Red", "Red", "Red"]) ➞ 13
+// Notes
+// Only change coloring pencils if the next color is different to the color before it.
+// Return a number in seconds.
+
+// function colorPatternTimes(arr){
+//     // to switch pencils - 1sec
+//     // color square - 2sec
+//     let timeSquare=arr.length*2
+//     let timeSwitch=0;
+
+//     for (let i=0; i<arr.length;i++){
+//     if(arr[i]!==arr[i-1]){
+//         timeSwitch++
+//     }
+//     }
+//     return timeSwitch-1+timeSquare
+
+// }
+// console.log(colorPatternTimes(["Red", "Blue", "Red", "Blue", "Red"]))
 // ===168===
 // Create a function which validates whether a 3 character string is a vowel sandwich. In order to have a valid sandwich, the string must satisfy the following rules:
 
