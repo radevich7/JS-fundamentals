ec102d777db700636116c9edb795aaed82174144
Julian Radevych <79290689+radevich7@users.noreply.github.com>
2022-01-18 07:53:38 -0700

    <Practise>

--- code.js
+++ code.js
@@ -1,5 +1,42 @@
 "use strict";
-// ===170===
+// ===172===
+// ===171===
+// One cause for speeding is the desire to shorten the time spent traveling. In long distance trips, speeding does save an appreciable amount of time. However, the same cannot be said about short distance trips.
+
+// Create a function that calculates the amount of time saved were you traveling with an average speed that is above the speed-limit as compared to traveling with an average speed exactly at the speed-limit.
+
+// Examples
+// // The parameter's format is as follows:
+// // (speed limit, avg speed, distance traveled at avg speed)
+
+// timeSaved(80, 90, 40) ➞ 3.3
+
+// timeSaved(80, 90, 4000) ➞ 333.3
+
+// timeSaved(80, 100, 40 ) ➞ 6.0
+
+// timeSaved(80, 100, 10) ➞ 1.5
+// Notes
+// Speed = distance/time
+// The time returned should be in minutes, not hours.
+// Round the answer to one decimal place.
+// The speed limit and average speed are both given in mi/hr
+
+// write a function and call it
+// calculate thethe time spend on the trip with avg speed
+// calculate thethe time spend on the trip with speed limit
+// compare them and return the value
+
+// const timeSaved=(speedLimit,avgSpeed,distance)=>{
+//     // speed=distance/time; time=distance/speed
+//     const speedLimitTime=distance/(speedLimit)
+//     const avgSpeedTime=distance/(avgSpeed)
+//     return ((speedLimitTime-avgSpeedTime)*60).toFixed(1)
+
+// };
+
+// console.log(timeSaved(80,90,40))
+// // ===170===
 // Write a function that, given a date (in the format MM/DD/YYYY), returns the day of the week as a string. Each day name must be one of the following strings: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", or "Saturday".
 
 // To illustrate, the day of the week for "12/07/2016" is "Wednesday".
