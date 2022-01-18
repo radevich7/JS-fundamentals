d5a4820717e9259b75e459f714f5bda71de02ced
Julian Radevych <79290689+radevich7@users.noreply.github.com>
2022-01-18 07:40:23 -0700

    <Date practise>

--- code.js
+++ code.js
@@ -1,4 +1,26 @@
 "use strict";
+// ===170===
+// Write a function that, given a date (in the format MM/DD/YYYY), returns the day of the week as a string. Each day name must be one of the following strings: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", or "Saturday".
+
+// To illustrate, the day of the week for "12/07/2016" is "Wednesday".
+
+// Examples
+// getDay("12/07/2016") ➞ "Wednesday"
+
+// getDay("09/04/2016") ➞ "Sunday"
+
+// getDay("12/08/2011") ➞ "Thursday"
+// Notes
+// This challenge assumes the week starts on Sunday.
+
+// const getDay=(str)=>{
+//     let days=['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+//     const day=new Date(str);
+//     let weekday=day.getDay()
+
+//     return days[weekday]
+// }
+// console.log(getDay('12/07/2016'))
 // ===169===
 // When coloring a striped pattern, you may start by coloring each square sequentially, meaning you spend time needing to switch coloring pencils.
 
