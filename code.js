397fd5060f73e62ddba210dd96a186717f676193
Julian Radevych <79290689+radevich7@users.noreply.github.com>
2022-01-19 09:05:09 -0700

    <parseFloat and toFixed practise>

--- code.js
+++ code.js
@@ -1,6 +1,46 @@
 // // "use strict";
 // // // ===175===
+// A half life is the amount of time for half of a radioactive substance to decay.
 
+// After 1 half life, 50% of a substance will be left.
+// After 2 half lives, 25% of a substance will be left.
+// After 3 half lives, 12.5% of a substance will be left, etc...
+// Create a function which calculates the remaining mass and the number of years that it took for the substance to decay. You will be given:
+
+// The mass of the substance.
+// The time in years for a halflife to elapse.
+// The number of half lives.
+// Worked Example
+// halflifeCalculator(1000, 5730, 2) ➞ [250, 11460]
+
+// // There are 2 half lives, so the mass decays from 1000 to 500, then from 500 to 250.
+// // Each halflife is 5730 years, and since there were 2, it took 11460 years in total.
+// Examples
+// halflifeCalculator(1600, 6, 3) ➞ [200, 18]
+
+// halflifeCalculator(13, 500, 1) ➞ [6.5, 500]
+
+// halflifeCalculator(100, 35, 5) ➞ [3.125, 175]
+// Notes
+// Round the final mass to three decimal places.
+// All inputs are positive numbers.
+// Return the final mass first, then the number of years.
+
+//  1 half life  50% => each half is divided by 2 for next half life
+// first argument: mass of substance
+// second argument : the time of the half life
+// third : the number of half lifes
+
+// const halfLifeCalc=(mass, timeElapse,quantityHalfs)=>{
+//     let weight=mass;
+//     for (let i=0;i<quantityHalfs;i++){
+//         weight/=2
+
+//     }
+//     return [parseFloat(weight.toFixed(3)),parseFloat((timeElapse*quantityHalfs).toFixed(3))]
+
+// }
+// console.log(halfLifeCalc(1600,6,3))
 // // // ===174===
 // Create a function that determines whether a shopping order is eligible for free shipping. An order is eligible for free shipping if the total cost of items purchased exceeds $50.00.
 
