044ad1e35e66ace939a0d368093a28ea1ac673e7
julian <julian@julians-MacBook-Pro.local>
2022-01-21 08:01:35 -0700

    <get Time practise>

--- code.js
+++ code.js
@@ -1,5 +1,32 @@
 // // "use strict";
+// // // ===176===
 
+// Create a function that takes two dates and returns the number of days between the first and second date.
+
+// Examples
+// getDays(
+//   new Date("June 14, 2019"),
+//   new Date("June 20, 2019")
+// ) ➞ 6
+
+// getDays(
+//   new Date("December 29, 2018"),
+//   new Date("January 1, 2019")
+// ) ➞ 3
+// // Dates may not all be in the same month/year.
+
+// getDays(
+//   new Date("July 20, 2019"),
+//   new Date("July 30, 2019")
+// ) ➞ 10
+// Notes
+// N/A
+
+// const getDays=(first,second)=>{
+//     return (second.getTime()-first.getTime())/1000/60/60/24
+// }
+
+// console.log(getDays(new Date('June 14, 2019'), new Date('June 20, 2019')))
 // // // ===175===
 // A half life is the amount of time for half of a radioactive substance to decay.
 
