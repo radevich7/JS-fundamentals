f851d3849cf8e23eaee80760fc51dffe79e543f7
Julian Radevych <radevich7@gmail.com>
2022-02-01 08:57:32 -0700

    <math.round practise>

--- code.js
+++ code.js
@@ -1,4 +1,25 @@
 // // "use strict";
+// ===194===
+// Given a number between 1-26, return what letter is at that position in the alphabet. Return "invalid" if the number given is not within that range, or isn't an integer.
+
+// Examples
+// letterAtPosition(1) ➞ "a"
+
+// letterAtPosition(26.0) ➞ "z"
+
+// letterAtPosition(0) ➞ "invalid"
+
+// letterAtPosition(4.5) ➞ "invalid"
+// Notes
+// Return a lowercase letter.
+// Numbers that end with ".0" are valid.
+
+// String.fromCharCode()
+
+// const letter=(num)=>{
+//     return String.fromCharCode(96 + Math.round(num))}
+
+// console.log(letter(2))
 // ===193===
 // Create a function that takes a number as its parameter and returns another function. The returned function must take an array of numbers as its parameter, and return an array of the numbers divided by the number that was passed into the first function.
 
