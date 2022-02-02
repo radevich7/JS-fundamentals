e57019a80690785da354e672bc10a7f4ba110909
Julian Radevych <radevich7@gmail.com>
2022-02-02 08:26:32 -0700

    <while loop>

--- code.js
+++ code.js
@@ -1,4 +1,30 @@
 // // "use strict";
+// ===195===
+// Given two integers a and b, return how many times a can be halved while still being greater than b.
+
+// Examples
+// halveCount(1324, 98) ➞ 3
+// // (1324 -> 662 -> 331 -> 165.5)
+
+// halveCount(624, 8) ➞ 6
+// // (624 -> 312 -> 156 -> 78 -> 39 -> 19.5 -> 9.75)
+
+// halveCount(1000, 3) ➞ 8
+// // (1000 -> 500 -> 250 -> 125 -> 62.5 -> 31.25 -> 15.625 -> 7.8125 -> 3.90625)
+// Notes
+// Integer a will always be able to be halved at least once in every test case.
+
+// const halves=(num1,num2)=>{
+//     let cur=num1;
+//     let i=-1;
+//     while (cur>num2){
+//         cur/=2;
+//         i++
+//     }
+//     return i;
+// }
+
+// console.log(halves(624,8))
 // ===194===
 // Given a number between 1-26, return what letter is at that position in the alphabet. Return "invalid" if the number given is not within that range, or isn't an integer.
 
