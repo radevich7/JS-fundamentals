4d796b2d5d33b66bf7651ddf94c8c10e3109f8c2
Julian Radevych <79290689+radevich7@users.noreply.github.com>
2022-02-14 09:40:50 -0700

    <Sorting array practise>

--- code.js
+++ code.js
@@ -1,4 +1,22 @@
 // // "use strict";
+// ===204===
+// Create a sorting function which sorts numbers not by numerical order, but by number length! This means sorting numbers with the least amount of digits first, up to the numbers with the most digits.
+
+// Examples
+// numberLenSort([1, 54, 1, 2, 463, 2]) ➞ [1, 1, 2, 2, 54, 463]
+
+// numberLenSort([999, 421, 22, 990, 32]) ➞ [22, 32, 999, 421, 990]
+
+// numberLenSort([9, 8, 7, 6, 5, 4, 31, 2, 1, 3]) ➞ [9, 8, 7, 6, 5, 4, 2, 1, 3, 31]
+// Notes
+// If two numbers have the same number of digits, return them in the order they first appeared (this makes it different to just sorting the numbers normally).
+
+// const sorting= (arr)=>{
+//   return arr.map(val=>val+'').sort((a,b)=>a.length-b.length).map(val=>+val)
+
+// }
+
+// console.log(sorting([999, 421, 22, 990, 32]))
 // ===203===
 // Write a function that removes all capital letters from a sentence except the first letter, puts quotation marks around the sentence and adds ", whispered Edabit." at the end.
 
